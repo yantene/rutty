588168a920a517f8912ccf7e5c87f37d7b3ddc46
yantene <contact@yantene.net>
2021-01-11 08:01:47 +0900

    Create RubyExecutor

---
+++ app/executors/ruby_executor.rb
@@ -0,0 +1,28 @@
+class RubyExecutor
+  def initialize(timeout_sec: 3, kill_sec: 5)
+    @executor = DockerSandbox.new(docker_image: "ruby:3.0-slim")
+    @timeout_sec = timeout_sec
+    @kill_sec = kill_sec
+  end
+
+  # Ruby のサンドボックスでコードを実行する。
+  # @param [String] code 実行する Ruby コード。
+  # @return [Array<String, String, Integer>] 標準出力、標準エラー出力、終了ステータスの配列。
+  def execute!(code)
+    Dir.mktmpdir do |dir|
+      script_path = File.join(dir, "script.rb")
+      File.open(script_path, "w", 0o644) do |f|
+        f.write(code)
+      end
+
+      stdout, stderr, status = @executor.run!(
+        %w[/usr/local/bin/ruby /script.rb],
+        timeout_sec: @timeout_sec,
+        kill_sec: @kill_sec,
+        mount_files: { script_path => "/script.rb" },
+      )
+
+      [stdout, stderr, status.exitstatus]
+    end
+  end
+end
