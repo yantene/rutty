bf051ff27c96f83209fb2c1f53d9c7021da874c2
yantene <contact@yantene.net>
2021-01-28 08:37:36 +0900

    ExecutorEnvironment::Php 対応

--- app/executors/executor_environment/php.rb
+++ app/executors/executor_environment/php.rb
@@ -1,33 +1,16 @@
 module ExecutorEnvironment
   module Php
+    extend Base
+
     LANGUAGE = "php"
     VERSION = "8.0.1"
 
     SCRIPT_PATH = "/script.php"
 
-    DOCKERFILE = <<~DOCKERFILE
-      FROM #{LANGUAGE}:#{VERSION}-cli-alpine
-      
-      RUN apk --update add sudo && rm -rf /var/cache/apk/*
-
-      RUN { \
-          echo "display_errors = stderr"; \
-      } > /usr/local/etc/php/php.ini
-      
-      RUN { \
-          echo "#!/bin/sh"; \
-          \
-          echo "chown nobody:nobody #{SCRIPT_PATH}"; \
-          echo "chmod 500 #{SCRIPT_PATH}"; \
-          \
-          echo "/usr/bin/sudo -u nobody \\\\"; \
-          echo "/usr/local/bin/php #{SCRIPT_PATH} \\\\"; \
-        } > /entry.sh && \
-        chmod 500 /entry.sh
-      
-      ENTRYPOINT "/entry.sh"
-    DOCKERFILE
-
-    IMAGE = Docker::Image.build(DOCKERFILE)
+    DOCKERFILE_DIR = File.join(__dir__, LANGUAGE)
+    DOCKERFILE_ARGS = {
+      LANGUAGE: LANGUAGE,
+      VERSION: VERSION,
+    }
   end
 end
